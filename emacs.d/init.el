f3327dfff3ba8031b938dae745853f003d138559
miy4 <miy4@users.noreply.github.com>
2016-02-15 16:54:08 +0900

    Migrate to helm-M-x

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -187,6 +187,7 @@
   (helm-mode 1)
 
   (global-set-key (kbd "C-c h") 'helm-mini)
+  (global-set-key (kbd "M-x") 'helm-M-x)
   (global-set-key (kbd "M-y") 'helm-show-kill-ring)
   (define-key helm-map (kbd "C-M-n") 'helm-next-source)
   (define-key helm-map (kbd "C-M-p") 'helm-previous-source)
