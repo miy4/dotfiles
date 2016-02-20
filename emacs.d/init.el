a205f93970c3ceddc1798d52d31e037903617e0b
miy4 <miy4@users.noreply.github.com>
2016-02-20 12:35:12 +0900

    Add trigger to yas-expand

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -282,6 +282,7 @@
   (with-eval-after-load 'yasnippet
     (define-key yas-minor-mode-map (kbd "<tab>") nil)
     (define-key yas-minor-mode-map (kbd "TAB") nil)
+    (define-key yas-minor-mode-map (kbd "<backtab>") 'yas-expand)
     (setq yas-snippet-dirs '("~/.emacs.d/snippets" "~/.emacs.d/site-snippets"))
     (yas-global-mode 1))
   ;; helm-c-yasnippet
