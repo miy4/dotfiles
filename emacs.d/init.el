796f88d46e399727789d0876e8ab5bbf4bea6f80
miy4 <miy4t7l@gmail.com>
2016-04-25 14:40:39 +0900

    Fix package-archives which lost melpa-stable

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -4,8 +4,8 @@
 
 ;; Package Manager
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 (package-initialize)
 
 ;; Package Configuration
